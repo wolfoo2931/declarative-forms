1b113fc02c0da5142992cb3944d0e5098841c2fc
Wolf Oliver <owolf@anynines.com>
2019-02-19 07:39:17 +0100

    extend constructor specs to cover another scenarios

--- spec/src/declarativ_form_spec.js
+++ spec/src/declarativ_form_spec.js
@@ -7,24 +7,21 @@ describe('DeclarativForms Object', () => {
 
             describe('array Entry', () => {
                 it('will be present in the form to be filled in by the user', () => {
-                    form = new DeclarativForm({
-                        fields: [
-                            { name: 'fieldOne' }
-                        ]
-                    })
-
+                    form = new DeclarativForm({fields: [ { name: 'fieldOne' } ]})
                     expect(form.getHTML()).toMatch(/<input name=('|")fieldOne('|")>/)
+
+                    form = new DeclarativForm({fields: [ { name: 'fieldOne' }, { name: 'secondOne' } ]})
+                    expect(form.getHTML()).toMatch(/<input name=('|")fieldOne('|")>/)
+                    expect(form.getHTML()).toMatch(/<input name=('|")secondOne('|")>/)
                 })
 
                 describe('name Field', () => {
                     it('specifies the name of the form field and will be used to identify the field value', () => {
-                        form = new DeclarativForm({
-                            fields: [
-                                { name: 'fieldOne' }
-                            ]
-                        })
-
+                        form = new DeclarativForm({fields: [ { name: 'fieldOne' } ]})
                         expect(form.getValues()).toEqual({fieldOne: ''})
+
+                        form = new DeclarativForm({fields: [ { name: 'fieldOne' }, { name: 'secondOne' } ]})
+                        expect(form.getValues()).toEqual({fieldOne: '', secondOne: ''})
                     })
                 })
             })
