7bfc759fcbde4bb77bcdd9174ec1844dfee27dfc
Wolf Oliver <owolf@anynines.com>
2019-02-24 09:29:32 +0100

    allow to filter for options in DlSelect

--- spec/features/dl_select_spec.js
+++ spec/features/dl_select_spec.js
@@ -31,6 +31,60 @@ describe('DlSelect', () => {
         expect(await inputField.getValue()).toEqual('java')
     })
 
+    describe('when typing', () => {
+        it('empties the existing text so I only see the text I entered', async () => {
+            var dlOptions = await $('dl-option'),
+                inputField = await $(inputFieldSelector),
+                body = await $('body')
+
+            await inputField.click()
+            await dlOptions.click()
+            expect(await inputField.getValue()).toEqual('JavaScript')
+
+            await inputField.click()
+            await browser.keys('xxx')
+            expect(await inputField.getValue()).toEqual('xxx')
+        })
+
+        it('filters the options according to the input', async () => {
+          var dlOptions = await $$('dl-option'),
+              filteredDlOptions = [],
+              inputField = await $(inputFieldSelector),
+              body = await $('body')
+
+              expect(dlOptions.length).toEqual(4)
+
+              await inputField.click()
+              await inputField.keys('java')
+
+              dlOptions = await $$('dl-option')
+
+              for(let i=0; i<dlOptions.length; i++) {
+                  if(await dlOptions[i].isDisplayed()) {
+                      filteredDlOptions.push(dlOptions[i])
+                  }
+              }
+
+              expect(filteredDlOptions.length).toEqual(2)
+        })
+
+        it('resets the value to the last valid choosen option when focused lost', async () => {
+            var dlOptions = await $('dl-option'),
+                inputField = await $(inputFieldSelector),
+                body = await $('body')
+
+            await inputField.click()
+            await dlOptions.click()
+            expect(await inputField.getValue()).toEqual('JavaScript')
+
+            await inputField.click()
+            await browser.keys('xxx')
+            expect(await inputField.getValue()).toEqual('xxx')
+            await body.click()
+            expect(await inputField.getValue()).toEqual('JavaScript')
+        })
+    })
+
     describe('when an option gets clicked', () => {
         it('hides the options', async () => {
             var dlOptions = await $('dl-option'),
