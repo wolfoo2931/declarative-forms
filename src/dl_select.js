5147f2d6520065b842ace309809567c1fa9d8f42
Oliver Wolf <wolfoo2931@gmail.com>
2019-02-24 09:29:32 +0100

    allow to filter for options in DlSelect

--- src/dl_select.js
+++ src/dl_select.js
@@ -101,7 +101,8 @@ class DlSelect extends HTMLElement {
         this.optionsWrapper.style.display = 'none'
         this.inputField.placeholder = 'Select ...'
         this.inputField.onfocus = () => { self.focus() }
-        this.inputField.onblur = (e) => { self.unfocus(); }
+        this.inputField.onblur = (e) => { self.unfocus() }
+        this.inputField.oninput = () => { self.filterOptions(this.inputField.value) }
         this.arrow.onclick = () => { self.inputField.focus() }
 
         this.arrow.innerHTML = '<path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>'
@@ -119,6 +120,18 @@ class DlSelect extends HTMLElement {
         })
     }
 
+    filterOptions(str) {
+        str = str.toLowerCase()
+
+        Array.prototype.forEach.call(this.optionsWrapper.children, (opt) => {
+            if(str === '' || opt.innerText.toLowerCase().includes(str)) {
+                opt.style.display = 'block'
+            } else {
+                opt.style.display = 'none'
+            }
+        })
+    }
+
     setValue(value) {
       this.inputField.value = value
       this.value = value
@@ -130,6 +143,8 @@ class DlSelect extends HTMLElement {
 
     focus() {
         this.classList.add('focused')
+        this.inputField.placeholder = this.getValue() || 'Select ...'
+        this.inputField.value = ''
         this.optionsWrapper.style.display = 'inline-block'
     }
 
