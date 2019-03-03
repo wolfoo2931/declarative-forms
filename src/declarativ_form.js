05fbda15f908b19225d46082ff03704d8ccfdaef
Oliver Wolf <wolfoo2931@gmail.com>
2019-03-03 08:40:39 +0100

    fix form values in callback function

--- src/declarativ_form.js
+++ src/declarativ_form.js
@@ -40,7 +40,7 @@ function DeclarativForm(attrs, onChangeCallback) {
             if(fieldElement.setValue) {
                 fieldElement.setValue(field.defaultValue)
             } else {
-                fieldElement.value = field.defaultValue
+                fieldElement.setAttribute('value', field.defaultValue)
             }
         }
     })
@@ -73,7 +73,10 @@ DeclarativForm.prototype = {
         var result  = {}
 
         this.fields.forEach(function(field) {
-            result[field.name] = field.domElement.value
+            result[field.name] = field.domElement.getAttribute('value')
+            if(!result[field.name]) {
+                result[field.name] = ''
+            }
         })
 
         return result
