8b2e43c035d0f1a103b9e90cdf3be3d9caa5d431
Oliver Wolf <wolfoo2931@gmail.com>
2023-11-05 19:20:16 +0100

    fix setValue when dl-select is still loading

--- src/declarativ_form.js
+++ src/declarativ_form.js
@@ -438,7 +438,7 @@ function DeclarativForm(attrs, onChangeCallback, onCancelCallback, confirmButton
         self.dom.children[0].appendChild(fieldWrapper)
 
         if(field.defaultValue) {
-            this.allPromises.push((Promise.resolve(self.initPromises[field.name])).then(_ => {
+            const promiseToSetDefaultValue = Promise.resolve(self.initPromises[field.name]).then(_ => {
                 let tmpDefaultValue = typeof field.defaultValue === 'function' ? field.defaultValue(self.formData) : field.defaultValue
 
                 if(fieldElement.setValue) {
@@ -448,7 +448,9 @@ function DeclarativForm(attrs, onChangeCallback, onCancelCallback, confirmButton
                 } else {
                     fieldElement.setAttribute('value', tmpDefaultValue)
                 }
-            }))
+            })
+
+            this.allPromises.push(promiseToSetDefaultValue)
         }
     })
 
